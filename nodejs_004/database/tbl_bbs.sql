f388d8bc823d4a32515a748e7195e2cf5a3dd588
wndeld777 <wndeld777@gmail.com>
2021-08-23 16:43:26 +0900

    게시판

--- nodejs_004/database/tbl_bbs.sql
+++ nodejs_004/database/tbl_bbs.sql
@@ -6,6 +6,7 @@ GRANT ALL privileges ON *.* TO 'node'@'%';
 
 CREATE DATABASE nodeDB;
 USE nodedb;
-
+-- nodejs 에서 primarykey 를 지정해 주지 않으면 자동으로 생성됨
 DESC tbl_bbs;
 DROP TABLE tbl_bbs;
+select * FROM tbl_bbs;
